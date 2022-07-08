fdc96bae05d45f259aab6660ad4d77f8e8c3dfd9
torres30 <tomastorresdiaz01@gmail.com>
2022-07-07 22:46:47 -0300

    file system 2

--- manejoDeArchivos.js
+++ manejoDeArchivos.js
@@ -1,3 +1,11 @@
+class Container {
+    constructor(title, price, url) {
+        this.title = title;
+        this.price = price;
+        this.thumbnail = url;
+    }
+}
+
 const fs = require("fs");
 
 function getAll() {    
@@ -7,15 +15,14 @@ function getAll() {
 
 function save(obj) {
     let products = getAll();
-    let last = products[products.length - 1]
-    products.forEach(product => {
-        if(product.id) {
-            obj.id = last.id + 1;
-        } else obj.id = 1; 
-    })
 
+    if (products.length) {
+        let last = products[products.length - 1]
+        obj.id = last.id + 1;
+    } else obj.id = 1
+    
     products.push(obj)
-    fs.writeFileSync("./products.json", JSON.stringify(products))
+    fs.writeFileSync("./products.json", JSON.stringify(products, null, 2))
     return obj.id
 }
 
@@ -27,25 +34,22 @@ function getById(id) {
 function deleteById(id) {
     let products = getAll();
     let newArray = products.filter(product => product.id != id)
-    fs.writeFileSync("./products.json", JSON.stringify(newArray))
+    fs.writeFileSync("./products.json", JSON.stringify(newArray, null, 2))
 }
 
 function deleteAll() {;
-    fs.writeFileSync("./products.json", "")
+    fs.writeFileSync("./products.json", "[]")
 }
 
+const sw = new Container("Nintendo Switch", 200,"https://m.media-amazon.com/images/I/41-a+qwZXgL._SX342_SY445_.jpg");
+const xb = new Container("Xbox Series S", 300,"https://m.media-amazon.com/images/I/71NBQ2a52CL._SL1500_.jpg");
+const ps = new Container("Playstation 5", 400,"https://m.media-amazon.com/images/I/619BkvKW35L._SX466_.jpg");
 
-/*
-    save(Object): Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
-    getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
-    getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.
-    deleteById(Number): void - Elimina del archivo el objeto con el id buscado.
-    deleteAll(): void - Elimina todos los objetos presentes en el archivo.
-
-    El método save incorporará al producto un id numérico, que deberá ser siempre uno más que el id del último objeto agregado (o id 1 si es el primer objeto que se agrega) y no puede estar repetido.
-    Tomar en consideración el contenido previo del archivo, en caso de utilizar uno existente.
-    Implementar el manejo de archivos con el módulo fs de node.js, utilizando promesas con async/await y manejo de errores.
-    Probar el módulo creando un contenedor de productos, que se guarde en el archivo: “productos.txt”
-    Incluir un llamado de prueba a cada método, y mostrando por pantalla según corresponda para verificar el correcto funcionamiento del módulo construído. 
-
-*/
+save(sw);
+save(xb);
+save(ps);
+console.log(getById(2));
+deleteById(2);
+console.log(getAll());
+deleteAll();
+console.log(getAll());
