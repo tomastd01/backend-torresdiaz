3905f5dc056c4e247a7d75c7f83c13e500ad5e3a
torres30 <tomastorresdiaz01@gmail.com>
2022-07-12 06:32:09 -0300

    file system correcion

--- manejoDeArchivos.js
+++ manejoDeArchivos.js
@@ -1,55 +1,98 @@
+const fs = require("fs/promises");
+
 class Container {
-    constructor(title, price, url) {
-        this.title = title;
-        this.price = price;
-        this.thumbnail = url;
+    constructor(name) {
+        this.fileName = name;
+        this.content = [];
     }
-}
 
-const fs = require("fs");
 
-function getAll() {    
-    let data = JSON.parse(fs.readFileSync("./products.json", "utf-8"));
-    return data
-}
+    async getAll() {
+        try {
+            return this.content;
+        } catch(err) {
+            console.log(err)
+        }  
+        
+    }
 
-function save(obj) {
-    let products = getAll();
-
-    if (products.length) {
-        let last = products[products.length - 1]
-        obj.id = last.id + 1;
-    } else obj.id = 1
+    async save(obj) {
+        try {
+            let products = this.content;
     
-    products.push(obj)
-    fs.writeFileSync("./products.json", JSON.stringify(products, null, 2))
-    return obj.id
+            if (products.length) {
+                let last = products[products.length - 1]
+                obj.id = last.id + 1;
+            } else obj.id = 1;
+
+            this.content.push(obj)
+            await fs.writeFile(this.fileName, JSON.stringify(this.content, null, 2))
+            return obj.id;
+
+        } catch (err){
+            console.log(err)
+        } 
+        
+    }
+
+    async getById(id) {
+        try {
+            return this.content.find(product => product.id == id);
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+
+    async deleteById(id) {
+        try {
+            let newArray = this.content.filter(product => product.id != id)
+            this.content = newArray;
+            await fs.writeFile(this.fileName, JSON.stringify(this.content, null, 2))
+        } catch {
+            console.log(err)
+        }
+    }
+
+    async deleteAll() {;
+        try {
+            this.content = []
+            await fs.writeFile(this.fileName, JSON.stringify(this.content))
+        } catch(err) {
+            console.log(err)
+        }
+    } 
 }
 
-function getById(id) {
-    let products = getAll();
-    return products.find(product => product.id == id);
+
+const sw = {title: "Nintendo Switch", price: 200, url: "https://m.media-amazon.com/images/I/41-a+qwZXgL._SX342_SY445_.jpg"};
+const xb = {title: "Xbox Series S", price: 300, url: "https://m.media-amazon.com/images/I/71NBQ2a52CL._SL1500_.jpg"};
+const ps = {title: "Playstation 5", price: 400, url:"https://m.media-amazon.com/images/I/619BkvKW35L._SX466_.jpg"};
+
+
+
+const file = new Container("./products.json")
+
+async function test() {
+    
+    let a = await file.save(sw);
+    let b = await file.save(xb);
+    let c = await file.save(ps);
+    let all = await file.getAll();
+    let objById = await file.getById(2);
+    
+
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(all)
+    await file.deleteById(3)
+    console.log(objById)
+    await file.deleteAll()
+
+    console.log(await file.getAll())
+
 }
 
-function deleteById(id) {
-    let products = getAll();
-    let newArray = products.filter(product => product.id != id)
-    fs.writeFileSync("./products.json", JSON.stringify(newArray, null, 2))
-}
-
-function deleteAll() {;
-    fs.writeFileSync("./products.json", "[]")
-}
-
-const sw = new Container("Nintendo Switch", 200,"https://m.media-amazon.com/images/I/41-a+qwZXgL._SX342_SY445_.jpg");
-const xb = new Container("Xbox Series S", 300,"https://m.media-amazon.com/images/I/71NBQ2a52CL._SL1500_.jpg");
-const ps = new Container("Playstation 5", 400,"https://m.media-amazon.com/images/I/619BkvKW35L._SX466_.jpg");
-
-save(sw);
-save(xb);
-save(ps);
-console.log(getById(2));
-deleteById(2);
-console.log(getAll());
-deleteAll();
-console.log(getAll());
+test()
